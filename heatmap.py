1ff074040c777008408b28426285793550dfdb17
Chouaki Bendahmane <2211552@cleaurendeau.qc.ca>
2023-09-11 18:00:01 -0400

    Hahahahaha on est mort

--- heatmap.py
+++ heatmap.py
@@ -1,22 +1,29 @@
 import plotly.express as px
+import pandas as pd
+from zipfile import ZipFile
 
-df = px.data.carshare()
-print(df['centroid_lon'])
-print(df['centroid_lat'])
-print(df)
+with ZipFile('gtfs_stm.zip') as myzip:
+    stops_df = pd.read_csv(myzip.open('stops.txt'))
 
+print(stops_df.head(1))
 
-fig = px.scatter_mapbox(df,
-                        lon = df['centroid_lon'],
-                        lat = df['centroid_lat'],
-                        zoom = 10,
-                        color = df['peak_hour'],
-                        size = df['car_hours'],
-                        width = 1200,
-                        height = 900,
-                        title = 'text',
-                        )
+def getSmallestLatLon(): # South-West
+    return stops_df[['stop_lat', 'stop_lon']].min()
 
-fig.update_layout(mapbox_style = 'open-street-map')
-fig.update_layout(margin = {'r':0, 't':50, 'l':0, 'b':10})
-fig.show()
+def getBiggestLatLon(): # North-East
+    return stops_df[['stop_lat', 'stop_lon']].max()
+
+def getStopWithLat(x):
+    return stops_df[stops_df.stop_lat == x]
+
+def getStopWithLon(y):
+    return stops_df[stops_df.stop_lon == y]
+
+def getStopFromLatLon(x, y):
+    return stops_df[(stops_df.stop_lat == x) & (stops_df.stop_lon == y)]
+
+def getNbStopsIn100x100m(x, y):
+    return stops_df[(stops_df.stop_lat > x -0.0005) & (stops_df.stop_lat < x + 0.0005) & (stops_df.stop_lon > y - 0.0005) & (stops_df.stop_lon < y + 0.0005)].shape[0]
+
+latLongmin = getSmallestLatLon()
+latLongmax = getBiggestLatLon()
